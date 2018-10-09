a34de8c670b39fe11954f971ac38bbff68fc9447
thornystefans <thorny17@ru.is>
2018-10-09 22:38:03 +0000

    Implemented Add for multiple numbers

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -11,3 +11,7 @@ it("should return number when only one number is in the string", () => {
 it("should return the sum of two given numbers", () => {
 	expect(add("1,2")).toBe(3);
 });
+
+it("should return the sum of multiple numbers", () => {
+	expect(add("1,2,3")).toBe(6);
+});
