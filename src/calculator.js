2690a6e06bdc78c7ccae46c0fab531b10ba6dae4
thornystefans <thorny17@ru.is>
2018-10-09 22:21:33 +0000

    Implemented Add for two numbers

--- src/calculator.js
+++ src/calculator.js
@@ -3,8 +3,14 @@ function add(number) {
 	if(number == "") {
 		return 0;
 	}
+	if(number.includes(",")) {
 
-	return parseInt(number);
+		var numberArray = number.split(",");
+		return parseInt(numberArray[0]) + parseInt(numberArray[1]);
+	}
+	else {
+		return parseInt(number);
+	}
 }
 
 module.exports = add;
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -7,3 +7,7 @@ it("should return zero on an empty string", () => {
 it("should return number when only one number is in the string", () => {
 	expect(add("5")).toBe(5);
 });
+
+it("should return the sum of two given numbers", () => {
+	expect(add("1,2")).toBe(3);
+});
